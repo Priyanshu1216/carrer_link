e6cf1110de631ba7ae176b31beb9892b6795d3f4
Pri-Nigam <prinigam2bestpeers.com>
2023-06-15 01:29:33 +0530

    Added devise and modified paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "jobs#index"
+  # root "jobs#index"
+  devise_scope :user do
+    root "devise/sessions#new"
+  end  
   resources :clients
   resources :jobs
 end
