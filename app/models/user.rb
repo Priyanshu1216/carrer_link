63c6e95fa6b526475c47906f7f1b78efe89684a6
Pri-Nigam <prinigam@bestpeers.com>
2023-06-15 12:17:37 +0530

    Mofified user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  after_initialize :default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum  role: [:applicant, :client]
+  def default_role
+    self.role ||= :applicant
+  end
+
+  enum  :role, applicant: 'applicant', client: 'client', suffix: true
 end
