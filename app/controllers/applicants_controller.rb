ce5e01f92c0f9521c597aae3363e9ba5fdf91a9f
Pri-Nigam <prinigam@bestpeers.com>
2023-06-16 19:14:47 +0530

    Modified controller and added new controllers

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,18 +1,15 @@
 class ApplicantsController < ApplicationController
   def new
-    byebug
     @user = User.find(params[:user_id])
     @job = @user.jobs.find(params[:id])
     @applicant = @job.applicants.new
   end
 
   def create
-    byebug
     @user = User.find(params[:user_id])
     @job = @user.jobs.find(params[:id])
     @applicant = @job.applicants.new(applicant_params)
     if @applicant.save
-      byebug
       redirect_to applicant_path(id: @applicant.id,job_id: @job.id,user_id: @user.id)
     else
       render :new
