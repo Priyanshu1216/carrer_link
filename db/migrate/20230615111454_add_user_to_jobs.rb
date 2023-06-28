ea12469554421886f8d99c87e0bbc36b8e2357e1
Pri-Nigam <prinigam@bestpeers.com>
2023-06-28 15:27:25 +0530

    Enhanced migration

--- db/migrate/20230615111454_add_user_to_jobs.rb
+++ db/migrate/20230615111454_add_user_to_jobs.rb
@@ -1,6 +1,6 @@
 class AddUserToJobs < ActiveRecord::Migration[7.0]
   def change
-    add_reference :jobs, :user, null: false, foreign_key: true
-    remove_column :jobs,  :client_id
+    # add_reference :jobs, :user, null: false, foreign_key: true
+    # remove_column :jobs,  :client_id
   end
 end
